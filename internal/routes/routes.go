1e2da8242e1d6773da3993eca6c384b6b86f5215
IloveNooodles <mgarebaldhie80@gmail.com>
2022-07-31 14:36:53 +0700

    feat: add routing

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,10 +2,17 @@ package routes
 
 import (
 	"database/sql"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func RoutesInit(server *gin.Engine, db *sql.DB){
-  
+func RoutesInit(server *gin.Engine, db *sql.DB) {
+	api := server.Group("/api")
+	v1 := api.Group("/v1")
+	v1.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hellow",
+		})
+	})
 }
