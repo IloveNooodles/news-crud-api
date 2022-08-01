519a9abf46ac0867ffdff315325780ea0df98e5e
IloveNooodles <mgarebaldhie80@gmail.com>
2022-08-01 15:36:11 +0700

    Feat: complete dockerfile with redis, app and migrations

--- pkg/lib/lib.go
+++ pkg/lib/lib.go
@@ -3,8 +3,6 @@ package lib
 import (
 	"database/sql"
 
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
 )
@@ -16,12 +14,6 @@ func Init(url string) {
 	if err != nil {
 		panic(err)
 	}
-
-  driver, err := postgres.WithInstance(db, &postgres.Config{})
-  m, err := migrate.NewWithDatabaseInstance(
-    
-  )
-
 	log.Info().Msg("Successfully connected to the database")
 	DB = db
 }
