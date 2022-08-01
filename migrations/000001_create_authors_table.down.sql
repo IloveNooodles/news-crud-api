2e552d583b2942a5217a82cfa1e8ec0407ffe5b8
IloveNooodles <mgarebaldhie80@gmail.com>
2022-08-01 18:36:24 +0700

    feat: change several config

--- migrations/000001_create_authors_table.down.sql
+++ migrations/000001_create_authors_table.down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE authors
-DROP CONSTRAINT articles_author_id_fkey;
+DROP CONSTRAINT IF EXISTS articles_author_id_fkey;
 
 DROP TABLE IF EXISTS authors;
--- migrations/000002_create_articles_table.down.sql
+++ migrations/000002_create_articles_table.down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE articles
-DROP CONSTRAINT articles_author_id_fkey;
+DROP CONSTRAINT IF EXISTS articles_author_id_fkey;
 
 DROP TABLE IF EXISTS articles;
