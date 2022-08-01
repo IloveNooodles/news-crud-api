2a48ea0a52e0b3eb36e4f3dae5ed9a5a9bcfa103
IloveNooodles <mgarebaldhie80@gmail.com>
2022-08-01 17:58:54 +0700

    feat: change ttl redis

--- internal/handler/articles.go
+++ internal/handler/articles.go
@@ -88,7 +88,7 @@ func (h *articlesHandler) GetArticles(c *gin.Context) {
 			return
 		}
 
-		err = rdb.Set(context.Background(), "articles", cahcedData, time.Second*5).Err()
+		err = rdb.Set(context.Background(), "articles", cahcedData, time.Minute * 2).Err()
 
 		if err != nil {
 			log.Info().Msg("failed to SET to redis")
--- internal/handler/authors.go
+++ internal/handler/authors.go
@@ -63,7 +63,7 @@ func (h *authorHandler) GetAuthors(c *gin.Context) {
 			return
 		}
 
-		err = rdb.Set(context.Background(), "authors", cachedData, time.Second*5).Err()
+		err = rdb.Set(context.Background(), "authors", cachedData, time.Minute*2).Err()
 
 		if err != nil {
 			log.Info().Msg("failed to SET to redis")
